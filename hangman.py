b1afbe1fb61c414089c2efb2fbf3a49704c52fae
Jeff <jeff.uca@gmail.com>
2020-07-25 16:45:41 +0200

    :sparkles: add hangman game 7/8 version

--- hangman.py
+++ hangman.py
@@ -1,17 +1,16 @@
 # Objectives
 #
-# The player starts the game with 8 "lives", that is our player can input the wrong letter 8 times.
-#
-#     Print No such letter in the word and reduce the attempts count if the word guessed by
-#     the program doesn't contain this letter.
-#     Print No improvements and reduce the attempts count if the guessed word contains this
-#     letter but the user tried this letter before.
-#     The attempts count should be decreased only if there are no letters to uncover.
+#     If the user enters the same letter twice then the program should output You already typed
+#     this letter .
+#     Also, you should check if the user prints an English lowercase letter or not. If not,
+#     the program should print It is not an ASCII lowercase letter .
+#     Also, you should check if the user printed exactly one letter. If not, the program
+#     should print You should input a single letter . Remember that zero is also not one!
+#     Note that all these three errors should not reduce attempts count!
 #
 # Please, make sure that your program's output formatting precisely follows the example output
 # formatting. Pay attention to the empty lines between tries and in the end.
-
-
+#
 import random
 
 
@@ -34,21 +33,24 @@ class Hangman:
             while tries < 8 and '-' in match_word:
                 print()
                 print(match_word)
-                letter = input('Input a letter')
-                print()
+                letter = input('Input a letter:')
 
-                if letter in letters_tried:
-                    tries += 1
-                    print('No improvements')
+                if len(letter) > 1 or len(letter) < 1:
+                    print('You should input a single letter')
+
+                elif letter in letters_tried:
+                    print('You already typed this letter')
+                elif not letter.isalpha() or letter.isupper():
+                    print('It is not an ASCII lowercase letter')
 
                 elif self.guess_word.find(letter) != -1:
-                    letters_tried.add(letter)
                     for i in range(len(self.guess_word)):
                         if self.guess_word[i] == letter:
                             match_word = match_word[:i] + letter + match_word[i + 1:]
                 else:
                     print('No such letter in the word')
                     tries += 1
+                letters_tried.add(letter)
 
             if '-' in match_word:
                 print('You are hanged!')
