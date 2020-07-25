4a6dcf5437996271efa97b667950d0ce795b69c7
Jeff <jeff.uca@gmail.com>
2020-07-25 15:06:49 +0200

    :sparkles: add hangman game 5/8 version

--- hangman.py
+++ hangman.py
@@ -1,8 +1,18 @@
 # Objectives
 #
-#     Create the following word list: 'python', 'java', 'kotlin', 'javascript'.
-#     Program the game to choose a random word from it. You can enter more words, but let's stick to these four for now.
-
+# Now your game should work the following way:
+#
+#     A player has exactly 8 tries and enters letters. If a player has more tries but he actually
+#     guessed the word, it doesn't mean anything.
+#     If the letter doesn't occur in the word, the computer takes one try away, even
+#     if the user already inputted this letter before.
+#     If the player doesn't have any more attempts, the game should end and the program should show a losing message.
+#     Otherwise, the player can continue to input letters.
+#     Also, use our previous word list: 'python', 'java', 'kotlin', 'javascript' so that your program can be tested
+#     more reliably.
+#
+# Please, make sure that your program's output formatting precisely follows the example output formatting.
+# Pay attention to the empty lines between tries and in the end.
 
 import random
 
@@ -19,12 +29,23 @@ class Hangman:
     def start_game(self):
         if self.words:
             self.guess_word = random.choice(self.words)
-            print('H A N G M A N')
-            guess = input('Guess the word {}:'.format(self.guess_word[:3] + '-' * (len(self.guess_word)-3)))
-            if guess == self.guess_word:
-                print('You survived!')
-            else:
-                print('You are hanged!')
+            print('H A N G M A N\n')
+            tries = 0
+            match_word = '-' * len(self.guess_word)
+            while tries < 8:
+                print(match_word)
+                letter = input('Input a letter\n')
+                if self.guess_word.find(letter) != -1:
+                    for i in range(len(self.guess_word)):
+                        if self.guess_word[i] == letter:
+                            match_word = match_word[:i] + letter + match_word[i + 1:]
+                            self.guess_word = self.guess_word[:i] + '_' + self.guess_word[i + 1:]
+                else:
+                    print('No such letter in the word\n')
+                tries += 1
+                print()
+
+            print('Thanks for playing!', "We'll see how well you did in the next stage", sep='\n')
         else:
             print('You have to add words before start a game.')
 
